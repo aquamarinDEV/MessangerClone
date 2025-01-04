49e0e08d45560179e6228b91d1f8e76f35c79fe7
Gabi <marin.gabriel99@yahoo.com>
2025-01-04 16:55:53 +0200

    Initial Commit

---
+++ MessangerApp/MessangerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessangerAppApp.swift
+//  MessangerApp
+//
+//  Created by Gabriel Marin on 04.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct MessangerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
